e5f5edfe8818ae2410bd79e157d6d0401e13c89e
nkshr <naokasa@outlook.jp>
2018-04-21 06:49:25 +0900

    Graph structure is under construction.

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,20 +1,67 @@
 #include <thread>
 #include <future>
+#include <mutex>
 
 #include "graph.h"
 
-bool Vertex::run() {
-	m_clock.start();
+mutex mtx;
 
+void Vertex::run() {
+	m_th = thread(&Vertex::run, this);
+}
+
+void Vertex::join() {
+	m_th.join();
+}
+
+void Vertex::processing_loop() {
+	m_clock.start();
+	
 	while (true) {
-		if (process())
+		mtx.lock();
+
+		mtx.unlock();
+		if (!m_brun)
+			break;
+
+		if (!process())
 			break;
 		
+
 		m_clock.adjust();
 	}
 }
 
+
+
 void Graph::run() {
+
 	for (int i = 0; i < m_num_vertexes; ++i) {
+		m_vertexes[i].run();
+	}
+
+	listen();
+}
+
+void Graph::listen() {
+
+}
+
+RadarSignal::RadarSignal(int buf_size) : m_idx(0), m_buf_size(buf_size){
+	m_buf = new double[m_buf_size];
+	m_times = new long long[m_buf_size];
+}
+
+RadarSignal::~RadarSignal() {
+	delete[] m_buf;
+	delete[] m_times;
+}
+
+void RadarSignal::set_signal(double d, long long t) {
+	m_buf[m_idx] = d;
+	m_times[m_idx] = t;
+
+	if (++m_idx == m_buf_size) {
+		m_idx = 0;
 	}
 }
--- src/graph.h
+++ src/graph.h
@@ -3,9 +3,14 @@
 
 class Vertex {
 public:
-	bool run();
+	void run();
+	void join();
+	void processing_loop();
 
 private:
+	bool m_brun;
+
+	thread m_th;
 	Clock m_clock;
 
 	virtual bool process() = 0;
@@ -19,12 +24,29 @@ public:
 	void run();
 	void add();
 	void remove();
+	template<typename T>
+	void set_edges(const char * vertex_name, const char * variable_name, T value);
+	void listen();
 
 private:
 	int m_num_vertexes;
 	int m_num_edges;
 
-	Vertex * vertexes;
-	Edge * Edges;
+	Vertex * m_vertexes;
+	Edge * m_edges;
 
+};
+
+class RadarSignal : protected Edge{
+public:
+	RadarSignal(int buf_size);
+	~RadarSignal();
+
+	void set_signal(double d, long long t);
+
+private:
+	int m_idx;
+	int m_buf_size;
+	double * m_buf;
+	long long * m_times;
 };
